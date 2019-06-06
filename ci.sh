0ddd0f451df99afa3e489d1a42c22875fec07137
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-06-06 07:16:02 +0100

    fix ci

--- ci.sh
+++ ci.sh
@@ -10,16 +10,20 @@ conda config --set always_yes yes --set show_channel_urls yes
 conda update conda
 conda env create -f environment.yml
 source activate testenvironment
+conda config --add channels conda-forge
 conda install pytest nbconvert nbval termcolor
 
 shopt -s globstar
 
+# This script runs, tests and converts (to html) all Jupyter notebooks in /notebooks
+# Probably running and converting should be turned off when the notebooks are ready for publication
+# notebooks with names starting with a dot . or an underscore _ are ignored.
 
-# command to run all notebooks in current folder and subfolders
+# command to run all notebooks
 jupyter nbconvert --ExecutePreprocessor.kernel_name=python3 --execute --inplace --allow-errors notebooks/**/[^_^.]*.ipynb
 
-#command to test all notebooks in current folder and subfolders
+#command to test all notebooks
 pytest --current-env --verbose --capture=no --nbval notebooks/**/[^_^.]*.ipynb
 
-#command to convert all notebooks in current folder and subfolders to html
+#command to convert all notebooks
 jupyter nbconvert notebooks/**/[^_^.]*.ipynb
