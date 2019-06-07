3380d623da0da7d4a66535cbc9f5ea4969b5a8e5
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-06-07 19:23:32 +0100

    removed _

--- ci.sh
+++ ci.sh
@@ -15,22 +15,22 @@ conda install pytest nbconvert nbval termcolor
 
 
 # This is to make sure double stars ** works
-shopt -s globstar extglob
+shopt -s globstar
 
 # This script runs, tests and converts (to html) all Jupyter notebooks in /notebooks
 # Probably running and converting should be turned off when the notebooks are ready for publication
-# notebooks with names starting with a dot . or an underscore _ or in such folders are ignored.
+# notebooks with names starting with a dot . or in a folder starting with a dot are ignored.
 
 cd notebooks
 
 # command to run all notebooks
-jupyter nbconvert --ExecutePreprocessor.kernel_name=python3 --execute --inplace --allow-errors **/[^_^.]*.ipynb
+jupyter nbconvert --ExecutePreprocessor.kernel_name=python3 --execute --inplace --allow-errors **/*.ipynb
 
 #command to convert all notebooks
-jupyter nbconvert **/[^_^.]*.ipynb
+jupyter nbconvert **/*.ipynb
 
 #command to test all notebooks
-pytest --current-env --verbose --capture=no --nbval **/[^_^.]*.ipynb
+pytest --current-env --verbose --capture=no --nbval **/*.ipynb
 
 
 
